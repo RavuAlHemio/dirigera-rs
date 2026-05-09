4823efac6d9bd383e0ee226f8d99f09cc12e50bc
Ondřej Hošek <ondra.hosek@gmail.com>
2026-05-09 16:26:44 +0200

    package updates (#5)
    Co-authored-by: Simon Sawert <simon@sawert.se>

--- src/lib.rs
+++ src/lib.rs
@@ -39,25 +39,54 @@ where
 /// A module that is used to disable TLS verification. This is used because the Dirigera HUB uses
 /// HTTPS but with a self signed certificate.
 pub mod danger {
+    #[derive(Debug)]
     pub struct NoCertificateVerification;
 
-    impl rustls::client::ServerCertVerifier for NoCertificateVerification {
+    impl rustls::client::danger::ServerCertVerifier for NoCertificateVerification {
         fn verify_server_cert(
             &self,
-            _end_entity: &rustls::Certificate,
-            _intermediates: &[rustls::Certificate],
-            _server_name: &rustls::client::ServerName,
-            _scts: &mut dyn Iterator<Item = &[u8]>,
+            _end_entity: &rustls::pki_types::CertificateDer,
+            _intermediates: &[rustls::pki_types::CertificateDer],
+            _server_name: &rustls::pki_types::ServerName,
             _ocsp_response: &[u8],
-            _now: std::time::SystemTime,
-        ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
-            Ok(rustls::client::ServerCertVerified::assertion())
+            _now: rustls::pki_types::UnixTime,
+        ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+            Ok(rustls::client::danger::ServerCertVerified::assertion())
+        }
+
+        fn verify_tls12_signature(
+            &self,
+            message: &[u8],
+            cert: &rustls::pki_types::CertificateDer<'_>,
+            dss: &rustls::DigitallySignedStruct,
+        ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+            let supported_algos = rustls::crypto::CryptoProvider::get_default()
+                .ok_or(rustls::Error::General("no crypto provider".into()))?
+                .signature_verification_algorithms;
+            rustls::crypto::verify_tls12_signature(message, cert, dss, &supported_algos)
+        }
+
+        fn verify_tls13_signature(
+            &self,
+            message: &[u8],
+            cert: &rustls::pki_types::CertificateDer<'_>,
+            dss: &rustls::DigitallySignedStruct,
+        ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+            let supported_algos = rustls::crypto::CryptoProvider::get_default()
+                .ok_or(rustls::Error::General("no crypto provider".into()))?
+                .signature_verification_algorithms;
+            rustls::crypto::verify_tls13_signature(message, cert, dss, &supported_algos)
+        }
+
+        fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+            rustls::crypto::CryptoProvider::get_default()
+                .map(|p| p.signature_verification_algorithms.supported_schemes())
+                .unwrap_or_default()
         }
     }
 
     pub fn tls_no_verify() -> rustls::ClientConfig {
         let mut tls = rustls::ClientConfig::builder()
-            .with_safe_defaults()
             .with_root_certificates(rustls::RootCertStore::empty())
             .with_no_client_auth();
 
